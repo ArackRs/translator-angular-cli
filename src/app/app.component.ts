aa592350e6e8891fd00a95a6ebbe93a3aa927034
Jack Arana <arack.dev@gmail.com>
2023-09-14 14:00:29 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dev-open-template-pc1';
+}
