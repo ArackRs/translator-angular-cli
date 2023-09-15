76c7a44cfbf2e19a37b6ec7d857718cfd34fd395
Jack Arana <arack.dev@gmail.com>
2023-09-15 01:29:31 -0500

    version 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './views/home/home.component';
-import { NotFoundComponent} from "./views/not-found/not-found.component";
+import { HomeComponent } from "./public/pages/home/home.component";
+import { NotFoundComponent } from "./public/pages/not-found/not-found.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './views/home/home.component';
-import { NotFoundComponent } from './views/not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './public/components/navbar/navbar.component';
 import { HttpClientModule } from "@angular/common/http";
-import {StudentsService} from "./learning/services/students.service";
+import { FooterComponent } from "./public/components/footer/footer.component";
+import { HomeComponent } from "./public/pages/home/home.component";
+import { NotFoundComponent } from "./public/pages/not-found/not-found.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    NavbarComponent,
     FooterComponent,
     HomeComponent,
     NotFoundComponent
@@ -35,7 +32,7 @@ import {StudentsService} from "./learning/services/students.service";
     MatCardModule,
     HttpClientModule
   ],
-  providers: [StudentsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
